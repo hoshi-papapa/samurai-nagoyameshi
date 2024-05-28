b17961156507b8352dee3984ad6d08d7b6a384bc
hoshi-papapa <danchicchichichi@gmail.com>
2024-05-28 21:13:38 +0900

    Revert "LaravelCahierをインストールした、会員登録機能を断念した"
    This reverts commit 3c99f253ac7a6947c7b67e653f93efe8804bac9a.

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -50,11 +50,11 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
-            'nickname' => ['required', 'string', 'max:255'],
-            'phone_number' => ['required', 'string', 'max:255'],
+            'nickname' => ['nullable', 'string', 'max:255'], // ニックネーム
+            'phone_number' => ['nullable', 'string', 'max:15'], // 電話番号
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'occupation' => ['required', 'string', 'max:255'],
-            'age' => ['required', 'integer'],
+            'occupation' => ['nullable', 'string', 'max:255'], // 職業
+            'age' => ['nullable', 'integer', 'min:0', 'max:150'], // 年齢
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
     }
@@ -74,8 +74,8 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'occupation' => $data['occupation'],
             'age' => $data['age'],
-            'subscription_end_date' => null, // デフォルト値を設定
-            'subscription_flag' => false, // デフォルト値を設定
+            'subscription_end_date' => null, // サブスク終了日
+            'subscription_flag' => false, // サブスクフラグ
             'password' => Hash::make($data['password']),
         ]);
     }
