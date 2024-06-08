f5a5a4400a53926101e30857e232f71ddc5e20f3
hoshi-papapa <danchicchichichi@gmail.com>
2024-06-08 21:59:13 +0900

    laravel-adminを削除

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,8 +22,8 @@ class DatabaseSeeder extends Seeder
         // ]);
         $this->call([
             CategoriesTableSeeder::class,
-            StoreCategorySeeder::class,
             StoreSeeder::class,
+            StoreCategorySeeder::class,
             UserSeeder::class,
         ]);
     }
